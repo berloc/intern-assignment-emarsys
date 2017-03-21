7e501debe286bbbb1004882a099a15a162ed9fc4
berloc <berloc1987@gmail.com>
2017-03-21 03:21:13 +0100

    changed sign of the return values

--- src/main/java/CityComparator.java
+++ src/main/java/CityComparator.java
@@ -6,12 +6,12 @@ class CityComparator implements Comparator<City> {
 
     @Override
     public int compare(City city1, City city2) {
-        System.out.println(city1.getName() + city2.getName());
-        if (city1.getDependency() != null || city2.getDependency() != null) {
-            if (city1.getDependency() != null && city1.getDependency().getName().equals(city2.getName())) {
-                return 1;
-            } else if (city2.getDependency() != null && city2.getDependency().getName().equals(city1.getName())) {
+//        System.out.println(city1.getName() + city2.getName());
+        if (city1.getRelation() != null || city2.getRelation() != null) {
+            if (city1.getRelation() != null && city1.getRelation().getName().equals(city2.getName())) {
                 return -1;
+            } else if (city2.getRelation() != null && city2.getRelation().getName().equals(city1.getName())) {
+                return 1;
             }
         }
         return 0;
