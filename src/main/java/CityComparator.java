29ba393b23f9007599c816b3a1c2cd766a68e7c6
berloc <berloc1987@gmail.com>
2017-03-21 05:36:28 +0100

    added try catch block to the method

--- src/main/java/CityComparator.java
+++ src/main/java/CityComparator.java
@@ -6,12 +6,14 @@ class CityComparator implements Comparator<City> {
 
     @Override
     public int compare(City city1, City city2) {
-        if (city1.getRelation() != null || city2.getRelation() != null) {
+        try {
             if (city1.getRelation() != null && city1.getRelation().getName().equals(city2.getName())) {
                 return -1;
             } else if (city2.getRelation() != null && city2.getRelation().getName().equals(city1.getName())) {
                 return 1;
             }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
         }
         return 0;
     }
