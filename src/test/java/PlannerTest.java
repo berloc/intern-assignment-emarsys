f2f8bf1c6eb3d3853a204d090075d15be3891144
berloc <berloc1987@gmail.com>
2017-03-21 02:06:01 +0100

    plan method tests for 1, 3without & 3with relation

--- src/test/java/PlannerTest.java
+++ src/test/java/PlannerTest.java
@@ -1,8 +1,60 @@
-import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-/**
- * Created by berloc on 2017.03.21..
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlannerTest {
 
+    private Planner planner;
+    private City u,v,w,x,y,z;
+    private List<City> cities;
+    private List<String> testList;
+
+    @Before
+    public void setUp() throws Exception {
+        planner = new Planner();
+        cities = new ArrayList<>();
+        u = new City("u");
+        z = new City("z");
+        w = new City("w");
+        v = new City("v");
+        x = new City("x");
+        y = new City("y");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        planner = null;
+        cities = null;
+    }
+
+    @Test
+    public void oneCity() {
+        cities.add(x);
+        assertEquals("x", planner.plan(cities).get(0));
+    }
+
+    @Test
+    public void threeCitiesWithoutDependency() {
+        cities.add(x);
+        cities.add(y);
+        cities.add(z);
+        testList = Arrays.asList(x.getName(), y.getName(), z.getName());
+        assertEquals(testList, planner.plan(cities));
+    }
+
+    @Test
+    public void threeCitiesWithDependency() {
+        cities.add(x);
+        cities.add(y);
+        cities.add(z);
+        z.setRelation(y);
+        testList = Arrays.asList(x.getName(), z.getName(), y.getName());
+        assertEquals(testList, planner.plan(cities));
+    }
 }
